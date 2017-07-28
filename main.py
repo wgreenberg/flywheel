b6f3867e8fa7fe090636b34b4db07c24664b9e61
Nick Kaufman <nick.kaufman.ucf@gmail.com>
2017-07-28 08:34:35 -0400

    200 status codes for querying assigned IDs. Wrapped up file io into separate function. uuid for uids. Didn't quite make it to temporary files - manually deleting all temporary files when shutting down flask server with ctrl+c

--- main.py
+++ main.py
@@ -1,52 +1,61 @@
-# Need to go back to file implementation...
-
 from lib import summarizer
 from flask import Flask, abort, jsonify, request
-from multiprocessing import Process, Queue, Array, Value
-import random
-import queue
+from multiprocessing import Process, Queue
 import json
+import queue
+import uuid
+import sys, traceback
 import os
 
 app = Flask(__name__)
 job_queue = Queue()
 
+def f_io(fn, mode, obj):
+    if mode == "r":
+        with open(fn, mode) as f:
+            return json.loads(f.read())
+    else:
+        with open(fn, mode) as f:
+            f.write(json.dumps(obj))
+
+def rm_files():
+    files = os.listdir("./data")
+    for f in files:
+        if f.endswith(".json"):
+            os.remove("./data/{}".format(f))
+
 def process_loop():
-    while True:
-        try:
+    try:
+        while True:
             job = job_queue.get()
-        except queue.Empty:
-            print("waiting...")
-            continue
-        summary = summarizer.summarize(job["data"])
-        fname="data/"+str(job["uid"])+".json"
-        with open(fname, "w+") as f:
-            f.write(json.dumps({"uid": job["uid"], "summary": summary, "summarized": True}))
+            obj = {"uid": job["uid"], "summary": summarizer.summarize(job["data"]), "summarized": True}
+            f_io("data/"+str(job["uid"])+".json", "w+", obj)
+    except KeyboardInterrupt:
+        rm_files()
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
+    sys.exit(0)
 
 @app.route("/api/submit", methods=["POST"])
 def submit():
     if not request.json or not "data" in request.json:
         abort(400)
-    uid = int("".join(str(random.choice(range(10))) for _ in range(10)))
+    uid = uuid.uuid4().int
     job = {"uid": uid, "data": request.json["data"], "summarized": False}
-    fname = "data/"+str(uid)+".json"
-    with open(fname, "w+") as f:
-        f.write(json.dumps(job))
+    f_io("data/{}.json".format(job["uid"]), "w+", job)
     job_queue.put(job)
     return jsonify({"response": job["uid"]}), 201
 
 @app.route("/api/query/<int:uid>", methods=["GET"])
 def query(uid):
     try:
-        fname = "data/"+str(uid)+".json"
-        with open(fname, "r") as f:
-            data = json.loads(f.read())
+        data = f_io("data/{}.json".format(uid), "r", None)
     except FileNotFoundError:
-        return jsonify({"response": "Invalid Key"}), 404
+        return jsonify({"status": "error"}), 404
     if data["summarized"]:
-        return jsonify({"response": data["summary"]}), 200
+        return jsonify({"response": data["summary"], "status": "succeeded"}), 200
     else:
-        return jsonify({"response": "Summarization has yet to complete"}), 203
+        return jsonify({"status": "pending"}), 200
 
 if __name__ == "__main__":
     p = Process(target=process_loop)
